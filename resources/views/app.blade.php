3d556423e6ab2aefbdd52066b2689d11bd7e91a1
forkanimahdi <forkanimahdi@gmail.com>
2026-04-01 19:37:46 +0100

    fix app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,7 +38,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx", "resources/js/pages/{$page['component']}.jsx"])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
         </x-inertia::head>
